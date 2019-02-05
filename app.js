5a58c333bb0dc99c5c07990c68e82b62c4a7e54b
Ricardo Vieira da Silva <ricardo.web@ultrafarma.com.br>
2019-02-05 15:16:09 -0200

    Novo commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Funciona perfeitamente
+// const http = require("http");
+
+// const server = http.createServer((request, response)=>{
+//     response.statusCode = 200;
+//     response.setHeader("Content-Type", "text/plain");
+//     response.end("Hello World!");
+// });
+
+//     server.listen(3000, (resquest, response)=>{
+//         console.log("Mais uma vez funcionou!");
+//     });
+
+const app = require("./custom-express")();
+
+app.listen(3000,(request, response)=>{
+    console.log("Funcionando bem...");
+})
